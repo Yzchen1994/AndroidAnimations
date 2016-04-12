5fb3c34c16246eff4e4291aa77fe45d917ca99d4
Macbook <yzchen1994@hotmail.com>
2016-04-12 12:08:39 -0400

    updated

--- app/src/main/java/com/example/macbook/animations/MainActivity.java
+++ app/src/main/java/com/example/macbook/animations/MainActivity.java
@@ -9,20 +9,31 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
 
+    public void fade(View view){
+        ImageView pic1 = (ImageView) findViewById(R.id.image);
 
+        //pic1.animate().rotation(180f).setDuration(2000);
+        //pic1.animate().scaleX(0.5f).scaleY(0.5f).setDuration(2000);
+        //pic1.animate().translationXBy(1000f).setDuration(2000);
+
+        pic1.animate().rotation(360f).translationXBy(1000f).setDuration(2000);
+
+        //ImageView pic2 = (ImageView) findViewById(R.id.image2);
+        //pic2.animate().alpha(1f).setDuration(2000);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void fade(View view){
+
         ImageView pic1 = (ImageView) findViewById(R.id.image);
-        ImageView pic2 = (ImageView) findViewById(R.id.image2);
+        pic1.setTranslationX(-1000f);
 
-        pic1.animate().alpha(0f).setDuration(2000);
 
-        pic2.animate().alpha(1f).setDuration(2000);
+
     }
+
+
 }
